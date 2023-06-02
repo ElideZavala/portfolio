966ab71e52005c86159aa5af14d4ca91b8acd572
Elide <zavalavinagreelide@gmail.com>
2023-06-02 17:31:43 -0600

    Loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <section className="relative w-full h-screen mx-auto">
             <div
-                className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+                className={`${styles.paddingX} absolute inset-0 top-[105px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
             >
                 <div className="flex flex-col justify-center items-center mt-5">
                     <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
@@ -15,11 +15,14 @@ const Hero = () => {
                 </div>
                 <div>
                     <h1 className={`${styles.heroHeadText}`}>
-                        Hi, I'm 
+                        Hi, I'm
                         <span className="text-[#915eff]">Elide</span>
-                        <p className={`${styles.heroSubText} mt-2 text-white-100 text-balance w-3/5`}>
-                          I am a Frontend Developer with a passion for design and technology, 
-                          I love to create beautiful and functional websites, and I am always looking for new challenges.
+                        <p
+                            className={`${styles.heroSubText} mt-2 text-white-100 text-balance w-3/4`}
+                        >
+                            I am a Frontend Developer with a passion for design and technology, I love to create beautiful and
+                            functional websites, and I am always looking for new
+                            challenges.
                         </p>
                     </h1>
                 </div>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import { Suspense, useEffect, useState } from "react"
 import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { OrbitControls, Preload, useGLTF, Html } from "@react-three/drei"
 
 import CanvasLoader from "../Loader"
 
@@ -8,12 +8,25 @@ const Computers = () => {
     const computer = useGLTF("./desktop_pc/scene.gltf")
 
   return (
-    <mesh>
+    <group>
       <hemisphereLight intensity={0.15} 
       groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
-    </mesh>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.7, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+        />
+    </group>
   )
 }
 
