e010166fdf0e2138682aedb9e36ec0dfcf0c716d
Elide <zavalavinagreelide@gmail.com>
2023-06-07 09:53:00 -0600

    work add tecnologis used in even project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -44,6 +44,19 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {
+            tags.map((tag) => (
+              <p 
+                key={tag.name} 
+                className={`name-[14px] ${tag.color}`}
+              >
+                  #{tag.name}
+              </p>
+            ))
+          }
+        </div>
       </Tilt>
     </motion.div>
   )
